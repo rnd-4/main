c1711cf1def67424f73362faa2754645bcd2f37e
NazarkoNaN <nazar.ivanyuk@gmail.com>
2023-03-02 19:12:34 -0800

    images work

--- Dormitories/Dormitories/settings.py
+++ Dormitories/Dormitories/settings.py
@@ -108,14 +108,19 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/image/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "image")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
--- Dormitories/hostels/urls.py
+++ Dormitories/hostels/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 from .views import *
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('dormitorie', dormitoriePage, name='dormitorie'),
 ]
+
