8fead1705f01b5a34ddc02663ff91656cdfbface
NazarkoNaN <nazar.ivanyuk@gmail.com>
2023-03-03 21:33:58 -0800

    dormitorie almost done

--- Dormitories/hostels/views.py
+++ Dormitories/hostels/views.py
@@ -15,21 +15,21 @@ def dormitoriePage(request):
             room = Room.objects.get(id=student.room.id)
             dormitorie = Hostel.objects.get(id=room.hostel_id)
             students = list(Student.objects.filter(room=room))
-
-            users=list()
-            for student in students:
-
-                user = User.objects.get(id=student.user.id)
-                print(user)
-                user_data={
-                    'firstname': user.first_name,
-                    'lastname': user.last_name,
-                    'email': user.email,
-                    'faculty': student.faculty,
-                    'gender': student.gender,
-                }
-                users.__add__(user_data)
-                print(user_data)
+            #
+            # users=list()
+            # for student in students:
+            #
+            #     user = User.objects.get(id=student.user.id)
+            #     print(user)
+            #     user_data={
+            #         'firstname': user.first_name,
+            #         'lastname': user.last_name,
+            #         'email': user.email,
+            #         'faculty': student.faculty,
+            #         'gender': student.gender,
+            #     }
+            #     users.__add__(user_data)
+            #     print(user_data)
 
             floor = room.floor
             number = room.number
@@ -39,7 +39,7 @@ def dormitoriePage(request):
                 'floor': floor,
                 'number': number,
                 'payment_status': payment_status,
-                'users': students,
+                'students': students,
             }
             print(data)
 
